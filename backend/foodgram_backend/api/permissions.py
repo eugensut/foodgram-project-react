d3b6b74c10213d3f2754664e5346c32df425c4f9
Евгений Долгор <otchet60@yandex.ru>
2024-03-04 23:01:56 +0300

    Review 1

--- backend/foodgram_backend/api/permissions.py
+++ backend/foodgram_backend/api/permissions.py
@@ -9,12 +9,10 @@ class RecipesPermissions(permissions.BasePermission):
             request.method in ('POST', 'PATCH')
             and request.user.is_authenticated
         )
-    
+
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in permissions.SAFE_METHODS
             or request.user.is_staff
             or obj.author == request.user
         )
-
-
