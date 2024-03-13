facd22064526531151263d0c2bf1ac1ebcdc4fcd
Евгений Долгор <otchet60@yandex.ru>
2024-03-13 17:42:00 +0300

    setting pagination

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -152,8 +152,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'PAGE_SIZE_QUERY_PARAM': 'limit',
 }
 DJOSER = {
     'LOGIN_FIELD': 'email',
