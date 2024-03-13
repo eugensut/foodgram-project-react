9c26aabfbfe06da2142e4e820a38537aa1f02f1b
Евгений Долгор <otchet60@yandex.ru>
2024-03-13 16:24:01 +0300

    Paginator

--- backend/foodgram_backend/api/views.py
+++ backend/foodgram_backend/api/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
@@ -156,6 +157,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = RecipeFilter
     permission_classes = [RecipesPermissions]
+    pagination_class = PageNumberPagination
 
     def get_serializer_class(self):
         if self.action == 'favorite':
